e1c64086ddc3c4d5287ed188c25bcbf0845b5c22
Jinal Patel <n01714049@humber.ca>
2025-02-14 15:39:45 -0500

    saved

--- week5/index.php
+++ week5/index.php
@@ -4,6 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        .colors{
+            width: 100%;
+            height: 80px;
+            display: flex;
+            justify-content: center;
+            align-items: center;            
+            font-weight: bold;
+        }
+    </style>
 </head>
 <body>
     <?php
@@ -14,9 +24,13 @@
             die("connection failed:" . mysqli_connect_error());
         }
         
-        $query = "SELECT Name FROM colors";
-        $colors = mysqli_query($connect, $query);
-        echo '<pre>' . print_r($colors) .'</pre>';
+        $query='SELECT * FROM colors';
+        $colors= mysqli_query($connect,$query);
+        // echo '<pre>'. print_r($colors). '</pre>';
+        foreach ($colors as $color)
+        {
+            echo '<div class="colors" style="background-color: ' . $color['Hex'] . ';">' . $color['Name'] . '</div>';
+        }
 
     ?>
 </body>
